d69b6b74093ef80844823bb97411729c8eff9ab5
Damien J <jorisdamien@gmail.com>
2023-04-06 21:48:22 +0200

    implements append deployment to empty lockfile

--- packster-core/src/operation/init_location.rs
+++ packster-core/src/operation/init_location.rs
@@ -1,6 +1,6 @@
 use std::path::{PathBuf, Path};
 
-use crate::{Result, path::Absolute, port::{ FileSystem, Parser, Serializer, ReadOnlyFileSystem, Archiver }, Error, domain::DeployLocation,  LOCKFILE_NAME};
+use crate::{Result, path::Absolute, port::{ FileSystem, Parser, Serializer, ReadOnlyFileSystem, Archiver }, Error, domain::{DeployLocation, Deployment},  LOCKFILE_NAME};
 use super::{Operation, New};
 
 pub struct InitLocationRequest {
@@ -24,7 +24,7 @@ pub struct LocationInitialized;
 
 //TODO proper error from filesystem if : lockfile is not an existing directory, and if location directory is a file
 impl InitLocationOperation<New> {
-    pub fn initialize_lockfile<F: FileSystem, S: Serializer>(self, filesystem: &F, serde: &S) -> Result<InitLocationOperation<LocationInitialized>> {
+    pub fn initialize_lockfile<F: FileSystem, S: Serializer>(self, filesystem: &F, serializer: &S) -> Result<InitLocationOperation<LocationInitialized>> {
         let lockfile_path = self.request.location_directory.as_ref().join(LOCKFILE_NAME);       
 
         abort_if_something_already_present(&lockfile_path, filesystem)?;
@@ -36,7 +36,7 @@ impl InitLocationOperation<New> {
         }
 
         let deploy_location = DeployLocation::default();
-        let deploy_location_file_content = serde.serialize(&deploy_location)?;
+        let deploy_location_file_content = serializer.serialize(&deploy_location)?;
         filesystem.write_all(&lockfile_path, deploy_location_file_content.as_bytes())?;
         Ok(Self::with_state(self.request, LocationInitialized))
     }
@@ -66,30 +66,32 @@ pub struct PackagedNotYetInstalled;
 
 
 
-pub fn install_package_to_location<P: AsRef<Path>, L: AsRef<Path>, F: FileSystem, A: Archiver>(path_package: P, path_location: L, filesystem: &F, archiver: &A) -> Result<()> {
-    // install package in location
-    // append to lockfile
-
-    // check if package is already installed in location
-    // if yes, abort
-    // if no, install package in location
-    let lockfile_path = path_location.as_ref().join(LOCKFILE_NAME);    
+pub fn install_package_to_location<P: AsRef<Path>, L: AsRef<Path>, F: FileSystem, A: Archiver, S: Serializer>(path_package: P, path_location: L, filesystem: &F, archiver: &A, serializer: &S) -> Result<()> {
+    // get checksum from package
     let checksum = extract_checksum(path_package.as_ref())?;
-    let deploy_path = path_location.as_ref().join(checksum);
+    // create deploy path from checksum to deploy package 
+    let deploy_path = path_location.as_ref().join(&checksum);
+    // install package in location
     archiver.extract(filesystem, path_package.as_ref(), deploy_path)?;
-
+    // get lockfile path
+    let lockfile_path = path_location.as_ref().join(LOCKFILE_NAME); 
+    // append deploy location to lockfile    
+    
+    // todo : check if package is already installed in location
+    // todo : if yes, abort
+    // todo : if no, install package in location
+    let mut deploy_location = DeployLocation::default();
+    let deployment: Deployment = Deployment::new(checksum);
+    deploy_location.add_deployment(deployment.clone());
+    
+    let deploy_location_file_content = serializer.serialize(&deploy_location)?;
+    
+    filesystem.write_all(lockfile_path, deploy_location_file_content.as_bytes())?;   
+    
     // read lockfile to string
     // parse lockfile
     // mutate lockfile ( add a deployment checksum )
-    // serialize and write mutated lockfile
-
-
-    
-    
-    let mut writer = filesystem.open_write(path_location.as_ref().join(LOCKFILE_NAME))?;
-
-    
-
+    // serialize and write mutated lockfile     
 
     // path_package : C:\\Downloads\static-package-a_0.0.1_ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad_f61f10025ad.packster
     // path_location : C:\\my-location
@@ -98,3 +100,47 @@ pub fn install_package_to_location<P: AsRef<Path>, L: AsRef<Path>, F: FileSystem
 
     Ok(())   
 }
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+}
+
+
+
+
+// let deploy_location = DeployLocation::default();
+
+// // Check if package is already installed in location
+// if package_is_installed_in_location(&deploy_location) {
+//     // If yes, abort
+//     println!("Package is already installed in location. Aborting.");
+//     return;
+// }
+
+// // If no, install package in location
+// if install_package_in_location(&deploy_location) {
+//     println!("Package installed successfully in location.");
+// } else {
+//     println!("Package installation failed.");
+// }
+
+
+// fn package_is_installed_in_location(location: &DeployLocation) -> bool {
+//     // TODO: Implement logic to check if package is installed in the specified location
+//     // For now, we'll assume the package is never installed and always return false
+//     false
+// }
+
+
+// fn package_is_installed_in_location(location: &DeployLocation) -> bool {
+//     // Check if any deployments exist in the location
+//     for deployment in location.as_slice() {
+//         // TODO: Implement logic to check if the package is installed in this deployment
+//         // For now, we'll assume the package is never installed and continue iterating
+//         // through the deployments.
+//     }
+//     // If we've iterated through all the deployments and haven't found the package, it's not installed.
+//     false
+// }
